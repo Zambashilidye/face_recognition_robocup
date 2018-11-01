38ccc753ce33c96631339cb33460736fa3560e60
snfgto <snfgto@outlook.com>
2018-11-01 20:18:33 +0800

    delete some files

--- src/python-speech-recognition/1.py
+++
@@ -1,6 +0,0 @@
-import speech_recognition as sr
-from guessing_game import recognize_speech_from_mic
-r = sr.Recognizer()
-m = sr.Microphone()
-recognize_speech_from_mic(r, m)  # speak after running this line
-#{'success': True, 'error': None, 'transcription': 'hello'}
