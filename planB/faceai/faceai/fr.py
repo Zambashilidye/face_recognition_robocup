3ff6e0ffa236292d174d7f759512b88c8e22d123
Cheng Tan <tangent916@foxmail.com>
2018-11-02 16:55:03 +0800

    Revert "Merge branch 'js'"
    This reverts commit 9e46df2698551217f4e9f3a946ca4d90f81af983, reversing
    changes made to 734603fd9b04243dd240d1b3a37c164d55a82d1d.

--- planB/faceai/faceai/fr.py
+++ planB/faceai/faceai/fr.py
@@ -72,7 +72,7 @@ while (fileNum<3):
             roi_color = frame[y:y+h, x:x+w]
             # 框出人脸
             y=y-h/4
-            h=h+h/3
+            h=h+h/4
             cv2.rectangle(frame, (x, y), (x + w, y +h), color, 2)
             waitTime=round(time.time()-startTime-takeTime[0],2)
             #text=str(runTime)
@@ -155,18 +155,17 @@ cmd_vel.publish(Twist())
 
 #######################################################
 ##in[3]  识别  ##
+'''
 if os.path.exists("photo.jpg"):
     os.remove("photo.jpg")
 if os.path.exists("seu.jpg"):
     os.remove("seu.jpg")
-
+'''
 startTime=time.time()
 path = "img/face_recognition"  # 模型数据图片目录
 cap = cv2.VideoCapture(1)
 cap.set(3,1920) #
 cap.set(4,1080)
-
-
 total_image_name = []
 total_face_encoding = []
 for fn in os.listdir(path):  #fn 表示的是文件名q
@@ -198,101 +197,84 @@ emotion_labels = {
     6: 'Calm'
 }
 
+
+
 ret, frame = cap.read()
-time.sleep(1)
-#runNum=0
-flag=0
-operatorNum=0
-faceNum=0
-lastfaceNum=0
-while(not flag):
-    operatorNum=0
-    ret, frame = cap.read()
-    time.sleep(1)
-    ret,frame=cap.read()
-    cv2.imwrite('photo.jpg',frame)
-    #cap.release()
+time.sleep(2)
+
+ret,frame=cap.read()
+#cv2.imwrite('photo.jpg',frame)
+cap.release()
+
+frame=cv2.imread('photo.jpg')
+
+#cv2.imshow('frame',frame)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+#Find operator
+face_locations = face_recognition.face_locations(frame)
+face_encodings = face_recognition.face_encodings(frame, face_locations)
+# 在这个视频帧中循环遍历每个人脸
+for (top, right, bottom, left), face_encoding in zip(
+        face_locations, face_encodings):
     
-    frame=cv2.imread('photo.jpg')
+    y=  top
+    x=  left
+    w=  right-left
+    h=  bottom-top
+    #y+h=    bottom
+    #x+w=    right
+    
+    # 框脸
+    #cv2.rectangle(frame, (left, top+h/4), (right, bottom+h/5), (0, 0, 200), 2)
+    # 标签
+    #cv2.rectangle(frame, (left, bottom), (right, bottom+h/5+30), (200, 200, 200),cv2.FILLED)
 
-    faceNum=0
-    #cv2.imshow('frame',frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #Find operator
-    face_locations = face_recognition.face_locations(frame)
-    face_encodings = face_recognition.face_encodings(frame, face_locations)
+    gray_face = gray[(top):(bottom), (left):(right)]
+    gray_face = cv2.resize(gray_face, (48, 48))
+    gray_face = gray_face / 255.0
+    gray_face = np.expand_dims(gray_face, 0)
+    gray_face = np.expand_dims(gray_face, -1)
+    emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+    emotion = emotion_labels[emotion_label_arg]
+    cv2.putText(frame, emotion, (left+5, bottom +40), cv2.FONT_HERSHEY_TRIPLEX, 1.0,
+                (0, 100, 100), 1)
 
-    for (top, right, bottom, left), face_encoding in zip(
-            face_locations, face_encodings):
-        faceNum=faceNum+1
-        y=  top
-        x=  left
-        w=  right-left
-        h=  bottom-top
-        #y+h=    bottom
-        #x+w=    right
-        
-        # 框脸
+    # gender
+    print(frame.shape)
+    face = frame[top-60:bottom+60, left-60:right+60]
+    #face = frame[top-h/3:bottom+h/3, left-w/3:right+w/3]  
+    cv2.imwrite('faceOperator.jpg',face)
+    face = cv2.resize(face, (48, 48))
+    face = np.expand_dims(face, 0)
+    face = face / 255.0
+    gender_label_arg = np.argmax(gender_classifier.predict(face))
+    gender = gender_labels[gender_label_arg]
+    #cv2.rectangle(frame, (left, top), (right, bottom),(0,255,255), 2)
+    cv2.putText(frame, gender, (left+5, bottom+20 ), cv2.FONT_HERSHEY_TRIPLEX, 1.0,
+                (100, 100, 0), 1)
+    
+    # operator
+    for i, v in enumerate(total_face_encoding):
+        match = face_recognition.compare_faces(
+            [v], face_encoding, tolerance=0.5)
+        name = " "
+        if match[0]:
+            name = total_image_name[i]
+            break
 
-        gray_face = gray[(top):(bottom+h/10), (left):(right)]
-        gray_face = cv2.resize(gray_face, (48, 48))
-        gray_face = gray_face / 255.0
-        gray_face = np.expand_dims(gray_face, 0)
-        gray_face = np.expand_dims(gray_face, -1)
-        emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
-        emotion = emotion_labels[emotion_label_arg]
-        
+    #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 255, 255),cv2.FILLED)
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    cv2.putText(frame, name, (left+5 , bottom+60), cv2.FONT_HERSHEY_TRIPLEX, 1.0,
+                (0, 0, 100), 1)
 
-        # gender
-        #print(frame.shape)
-        face = frame[top-60:bottom+60, left-60:right+60]
-        cv2.imwrite('faceOperator.jpg',face)
-        face = cv2.resize(face, (48, 48))
-        face = np.expand_dims(face, 0)
-        face = face / 255.0
-        gender_label_arg = np.argmax(gender_classifier.predict(face))
-        gender = gender_labels[gender_label_arg]
-        #cv2.rectangle(frame, (left, top), (right, bottom),(0,255,255), 2)
-        
-        
-        # operator
-        for i, v in enumerate(total_face_encoding):
-            match = face_recognition.compare_faces(
-                [v], face_encoding, tolerance=0.4)
-            name = " "
-            if match[0]:
-                name = total_image_name[i]
-                operatorNum=operatorNum+1
-                break
-
-        #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 255, 255),cv2.FILLED)
-        font = cv2.FONT_HERSHEY_TRIPLEX
-        
-
-        cv2.rectangle(frame, (left, top-h/4), (right, bottom+h/10), (0, 0, 200), 2)
-        # 标签
-        cv2.rectangle(frame, (left-1, bottom+h/10), (right, bottom+h/10+88), (200, 200, 200),cv2.FILLED)
-        cv2.putText(frame, gender, (left+5, bottom+h/10+22 ), cv2.FONT_HERSHEY_TRIPLEX, 1.0,
-                    (100, 100, 0), 1)
-        cv2.putText(frame, emotion, (left+5, bottom+h/10+44), cv2.FONT_HERSHEY_TRIPLEX, 1.0,
-                    (0, 100, 100), 1)
-        cv2.putText(frame, name, (left+5 , bottom+h/10+66), cv2.FONT_HERSHEY_TRIPLEX, 1.0,
-                    (0, 0, 100), 1)
-
-    #when False, continue
-    flag= ((operatorNum==1) and (lastfaceNum==faceNum))
-    print("operatorNum: "+str(operatorNum))
-    print("lastfaceNum: "+str(lastfaceNum))
-    print("faceNum:  "+str(faceNum))
-    print("flag: "+str(flag))
-
-    lastfaceNum=faceNum
 
 image = face_recognition.load_image_file("photo.jpg")
 
 #查找图像中所有面部的所有面部特征
 face_landmarks_list = face_recognition.face_landmarks(image)
-#print("face_load ")
+print("face_load ")
 for face_landmarks in face_landmarks_list:
     facial_features = [
         'chin',  # 下巴  type(face_landmarks_list[0]['nose_tip'])
@@ -323,16 +305,15 @@ for face_landmarks in face_landmarks_list:
             #bottom=np.max(cnt, axis=0)[1]
 
         cv2.rectangle(frame,(left-5,top-5),(right+5,bottom+5),(200,200,200),1)
-            #if facial_feature=='right_eye':
-            #    d.line(face_landmarks[facial_feature], fill=(255, 0, 0), width=2)
-            #else:
-            #    d.line(face_landmarks[facial_feature], fill=(255, 255, 255), width=2)
-        #pil_image.show()
-    #x,y,w,h = cv2.boundingRect(cnt)
-    #img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    
+        #if facial_feature=='right_eye':
+        #    d.line(face_landmarks[facial_feature], fill=(255, 0, 0), width=2)
+        #else:
+        #    d.line(face_landmarks[facial_feature], fill=(255, 255, 255), width=2)
+    #pil_image.show()
+#x,y,w,h = cv2.boundingRect(cnt)
+#img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
 cv2.imwrite('seu.jpg',frame)
-cap.release()
 endTime=time.time()
 print(endTime-startTime)
 time.sleep(3)
